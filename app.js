218388d07add94071d9474904f77f3d2280fee2d
Bruno Cavilla <bcavilla@gmail.com>
2022-10-19 15:36:36 -0300

    subiendo mLiebre

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const path = require('path');
+
+
+const port = process.env.PORT || 3001;
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto $(port)`));
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+});
+app.get('/register', (req, res) =>{
+    res.sendFile(path.resolve('views/register.html'))
+})
+app.post('/', (req, res) =>{
+    
+})
+app.get('/login', (req, res) =>{
+    res.sendFile(path.resolve('views/login.html'))
+})
